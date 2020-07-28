7991842b7baf7b935781d48048a75db731bab79e
Yuriy Sountsov <yuriys@kentico.com>
2020-07-28 16:40:16 -0400

    Extract translation method

--- Functions/Webhooks/KontentWebhook.cs
+++ Functions/Webhooks/KontentWebhook.cs
@@ -10,6 +10,7 @@ using Core;
 using KenticoKontent;
 using KenticoKontent.Models;
 using KenticoKontent.Models.Management.Elements;
+using KenticoKontent.Models.Webhook;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -61,91 +62,17 @@ namespace Functions.Webhooks
                     throw new ArgumentNullException(nameof(data.Items));
                 }
 
-                var translationLanguage = language.Substring(0, 2);
-
                 switch (message.Type)
                 {
                     case "content_item_variant":
                         switch (message.Operation)
                         {
                             case "change_workflow_step":
+                                var translationLanguage = language.Substring(0, 2);
+
                                 foreach (var item in data.Items)
                                 {
-                                    if (item.Item == null)
-                                    {
-                                        throw new ArgumentNullException(nameof(item.Item));
-                                    }
-
-                                    var languageVariant = await kontentRepository.RetrieveLanguageVariant(new RetrieveLanguageVariantParameters
-                                    {
-                                        ItemId = item.Item.Id,
-                                        LanguageId = item.Language?.Id
-                                    });
-
-                                    foreach (var element in languageVariant.Elements)
-                                    {
-                                        switch (element)
-                                        {
-                                            case RichTextElement richTextElement:
-                                                var value = richTextElement.Value;
-
-                                                if (value?.Length >= 5000)
-                                                {
-                                                    var parts = textAnalyzer.SplitHtml(value);
-                                                    var result = "";
-
-                                                    foreach (var part in parts)
-                                                    {
-                                                        var (partTranslated, partTranslation) = await Translate(part, translationLanguage);
-
-                                                        if (partTranslated)
-                                                        {
-                                                            result += partTranslation;
-                                                        };
-                                                    }
-
-                                                    if (!string.IsNullOrWhiteSpace(result))
-                                                    {
-                                                        richTextElement.Value = result;
-                                                    }
-
-                                                    break;
-                                                }
-
-                                                var (translated, translation) = await Translate(richTextElement.Value, translationLanguage);
-
-                                                if (translated)
-                                                {
-                                                    richTextElement.Value = translation;
-                                                };
-                                                break;
-
-                                            case UrlSlugElement urlSlugElement:
-                                                (translated, translation) = await Translate(urlSlugElement.Value, translationLanguage);
-
-                                                if (translated)
-                                                {
-                                                    urlSlugElement.Value = translation.Replace(" ", "-");
-                                                };
-                                                break;
-
-                                            case TextElement textElement:
-                                                (translated, translation) = await Translate(textElement.Value, translationLanguage);
-
-                                                if (translated)
-                                                {
-                                                    textElement.Value = translation;
-                                                };
-                                                break;
-                                        }
-                                    }
-
-                                    await kontentRepository.UpsertLanguageVariant(new UpsertLanguageVariantParameters
-                                    {
-                                        ItemId = item.Item.Id,
-                                        Language = language,
-                                        Variant = languageVariant
-                                    });
+                                    await TranslateItem(item, language, translationLanguage);
                                 }
                                 break;
                         }
@@ -166,6 +93,85 @@ namespace Functions.Webhooks
             {
                 return LogException(ex);
             }
+        }
+
+        private async Task TranslateItem(ItemObject item, string language, string translationLanguage)
+        {
+            if (item.Item == null)
+            {
+                throw new ArgumentNullException(nameof(item.Item));
+            }
+
+            var languageVariant = await kontentRepository.RetrieveLanguageVariant(new RetrieveLanguageVariantParameters
+            {
+                ItemId = item.Item.Id,
+                LanguageId = item.Language?.Id
+            });
+
+            foreach (var element in languageVariant.Elements)
+            {
+                switch (element)
+                {
+                    case RichTextElement richTextElement:
+                        var value = richTextElement.Value;
+
+                        if (value?.Length >= 5000)
+                        {
+                            var parts = textAnalyzer.SplitHtml(value);
+                            var result = "";
+
+                            foreach (var part in parts)
+                            {
+                                var (partTranslated, partTranslation) = await Translate(part, translationLanguage);
+
+                                if (partTranslated)
+                                {
+                                    result += partTranslation;
+                                };
+                            }
+
+                            if (!string.IsNullOrWhiteSpace(result))
+                            {
+                                richTextElement.Value = result;
+                            }
+
+                            break;
+                        }
+
+                        var (translated, translation) = await Translate(richTextElement.Value, translationLanguage);
+
+                        if (translated)
+                        {
+                            richTextElement.Value = translation;
+                        };
+                        break;
+
+                    case UrlSlugElement urlSlugElement:
+                        (translated, translation) = await Translate(urlSlugElement.Value, translationLanguage);
+
+                        if (translated)
+                        {
+                            urlSlugElement.Value = translation.Replace(" ", "-");
+                        };
+                        break;
+
+                    case TextElement textElement:
+                        (translated, translation) = await Translate(textElement.Value, translationLanguage);
+
+                        if (translated)
+                        {
+                            textElement.Value = translation;
+                        };
+                        break;
+                }
+            }
+
+            await kontentRepository.UpsertLanguageVariant(new UpsertLanguageVariantParameters
+            {
+                ItemId = item.Item.Id,
+                Language = language,
+                Variant = languageVariant
+            });
         }
 
         private async Task<(bool, string)> Translate(string? original, string translationLanguage)
